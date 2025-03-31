645e275527bb76e5e7f1d0e246115956c75f4c74
Novice Engineer <skyler32702@gmail.com>
2025-03-30 22:01:03 -0500

    Late Night Commit

--- fp_gpu/fp_gpu.srcs/sources_1/new/gpu_controller.v
+++ fp_gpu/fp_gpu.srcs/sources_1/new/gpu_controller.v
@@ -41,34 +41,76 @@ module gpu_controller #(
     input wire m_axi_hpr_rvalid,
     output reg m_axi_hpr_rready,
     input wire [31:0] m_axi_hpr_rdata,
-    input wire m_axi_hpr_rlast,
 
     // Master write interface
-    output wire m_axi_hpw_awvalid,
+    output reg m_axi_hpw_awvalid,
     input wire m_axi_hpw_awready,
-    output wire [31:0] m_axi_hpw_awaddr,
-    output wire [1:0] m_axi_hpw_awburst,
-    output wire [2:0] m_axi_hpw_awsize,
-    output wire [3:0] m_axi_hpw_awlen,
-    output wire m_axi_hpw_wvalid,
+    output reg [31:0] m_axi_hpw_awaddr,
+    output reg [1:0] m_axi_hpw_awburst,
+    output reg [2:0] m_axi_hpw_awsize,
+    output reg [3:0] m_axi_hpw_awlen,
+    output reg m_axi_hpw_wvalid,
     input wire m_axi_hpw_wready,
-    output wire [31:0] m_axi_hpw_wdata,
-    output wire [3:0] m_axi_hpw_wstrb,
-    output wire m_axi_hpw_wlast,
+    output reg [31:0] m_axi_hpw_wdata,
+    output reg [3:0] m_axi_hpw_wstrb,
+    output reg m_axi_hpw_wlast,
     input wire m_axi_hpw_bvalid,
-    output wire m_axi_hpw_bready
+    output reg m_axi_hpw_bready
 );
 
-localparam [2:0] IDLE_READ_STATE = 2'b000;
-localparam [2:0] ADDR_READ_STATE = 2'b001;
-localparam [2:0] DATA_READ_STATE = 2'b010;
-localparam [2:0] DONE_READ_STATE = 2'b011;
+localparam [1:0] IDLE_STATE = 2'b00;
+localparam [1:0] ADDR_STATE = 2'b01;
+localparam [1:0] ACTION_STATE = 2'b10;
+localparam [1:0] DONE_STATE = 2'b11;
 
-localparam [2:0] IDLE_WRITE_STATE = 2'b000;
-localparam [2:0] ADDR_WRITE_STATE = 2'b001;
-localparam [2:0] DATA_WRITE_STATE = 2'b010;
-localparam [2:0] DONE_WRITE_STATE = 2'b011;
+reg [1:0] rw_state = IDLE_STATE;
 
-reg [2:0] state = IDLE_READ_STATE;
+task init_signals ();
+    begin
+        rw_state = IDLE_STATE;
+        
+        new_screen_size = 1'b0;
+        new_projection_matrix = 1'b0;
+        new_clear_color = 1'b0;
+        new_camera_position = 1'b0;
+        new_camera_rotation = 1'b0;
+        new_model_position = 1'b0;
+        new_model_rotation = 1'b0;
+        new_model_scale = 1'b0;
+        new_mesh_data = 1'b0;
+        model_should_render = 1'b0;
+        clear_frame = 1'b0;
+
+        m_axi_hpr_arvalid = 1'b0;
+        m_axi_hpr_araddr = 32'h00000000;
+        m_axi_hpr_arburst = 2'b00;
+        m_axi_hpr_arsize = 3'b000;
+        m_axi_hpr_arlen = 4'b0000;
+        m_axi_hpr_rready = 1'b0;
+
+        m_axi_hpw_awvalid = 1'b0;
+        m_axi_hpw_awaddr = 32'h00000000;
+        m_axi_hpw_awburst = 2'b00;
+        m_axi_hpw_awsize = 3'b000;
+        m_axi_hpw_awlen = 4'b0000;
+        m_axi_hpw_wvalid = 1'b0;
+        m_axi_hpw_wdata = 32'h00000000;
+        m_axi_hpw_wstrb = 4'b0000;
+        m_axi_hpw_wlast = 1'b0;
+        m_axi_hpw_bready = 1'b0;
+    end
+endtask
+
+initial begin
+    init_signals();
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        init_signals();
+    end else begin
+
+    end
+end
 
 endmodule
